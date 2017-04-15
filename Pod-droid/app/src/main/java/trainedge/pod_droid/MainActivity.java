82a44d6ff3cfd4a8415bfae0385056bf7b2c67a3
icyhsr <hsrivastava558@gmail.com>
2017-04-15 17:30:24 +0530

    changed intent activity

--- Pod-droid/app/src/main/java/trainedge/pod_droid/MainActivity.java
+++ Pod-droid/app/src/main/java/trainedge/pod_droid/MainActivity.java
@@ -1,5 +1,6 @@
 package trainedge.pod_droid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,14 +17,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Intent mainIntent = new Intent(MainActivity.this, SoundActivity.class);
+        startActivity(mainIntent);
     }
 
 }
